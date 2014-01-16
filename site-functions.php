741b956d2a54f82dc064bad0af40f06af57e111e
Iain <iain@polevaultweb.com>
2014-01-16 11:41:51 +0000

    Removing Nivo Slider example code

--- site-functions.php
+++ site-functions.php
@@ -27,15 +27,3 @@ function example_action() {
 */
 
 // Add your code and snippets below
-
-add_filter( 'nivoslider_settings_page_header', 'my_nivoslider_settings_page_header' );
-
-function my_nivoslider_settings_page_header() {
-	return 'Client Slider Settings';
-}
-
-add_filter( 'nivoslider_settings_intro', 'my_nivoslider_settings_intro' );
-
-function my_nivoslider_settings_intro() {
-	return 'Here you can do this and that...';
-}
